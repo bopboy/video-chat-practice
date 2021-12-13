e4459f655aa8a4803156db74f5f1c4e0d7a97699
bopboy2 <scp0000@gmail.com>
2021-12-14 08:47:18 +0900

    카메라 변경

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,12 @@ const getCameras = async () => {
     try {
         const devices = await navigator.mediaDevices.enumerateDevices()
         const cameras = devices.filter(device => device.kind === "videoinput")
+        const currentCamera = myStream.getVideoTracks()[0]
         cameras.forEach(camera => {
             const option = document.createElement("option")
             option.value = camera.deviceId
             option.innerText = camera.label
+            if (currentCamera.label === camera.label) option.selected = true
             cameraSelect.appendChild(option)
         })
     } catch (e) {
@@ -24,13 +26,15 @@ const getCameras = async () => {
     }
 }
 
-const getMedia = async () => {
+const getMedia = async (deviceId) => {
+    const initialConstraints = { audio: true, video: { facingMode: "user" } }
+    const cameraConstraints = { audio: true, video: { deviceId: { exact: deviceId } } }
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true, video: true
-        })
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstraints
+        )
         myFace.srcObject = myStream
-        await getCameras()
+        if (!deviceId) await getCameras()
     } catch (e) {
         console.log(e)
     }
@@ -59,3 +63,7 @@ const handleCameraClick = () => {
     }
 }
 cameraBtn.addEventListener("click", handleCameraClick)
+const handleCameraChange = async () => {
+    await getMedia(cameraSelect.value)
+}
+cameraSelect.addEventListener("input", handleCameraChange)
