8f7c55c19509c971d9bd0d7ce9e1ec12788041be
bopboy2 <scp0000@gmail.com>
2021-12-13 20:17:10 +0900

    닉네임 설정 1 (메시지 타입 구분)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,12 +1,19 @@
 const socket = new WebSocket(`ws://${window.location.host}`)
 const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const messageForm = document.querySelector("#message")
+const nickForm = document.querySelector("#nick")
 
+const makeMessage = (type, payload) => {
+    const msg = { type, payload }
+    return JSON.stringify(msg)
+}
 socket.addEventListener("open", (message) => {
     console.log("Connected To Server ✅")
 })
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data)
+    const li = document.createElement("li")
+    li.innerText = message.data
+    messageList.append(li)
 })
 socket.addEventListener("close", () => {
     console.log("Disconnected from server ❌")
@@ -14,6 +21,11 @@ socket.addEventListener("close", () => {
 messageForm.addEventListener("submit", (event) => {
     event.preventDefault()
     const input = messageForm.querySelector("input")
-    socket.send(input.value)
+    socket.send(makeMessage("new_message", input.value))
     input.value = ''
+})
+nickForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const input = nickForm.querySelector("input")
+    socket.send(makeMessage("nickname", input.value))
 })
