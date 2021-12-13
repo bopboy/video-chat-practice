52f180112147941e8e166d09c94e98669c7b7c03
bopboy2 <scp0000@gmail.com>
2021-12-14 06:39:42 +0900

    방 채팅 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,15 +3,25 @@ const socket = io()
 const welcome = document.querySelector("#welcome")
 const form = welcome.querySelector("form")
 const room = document.querySelector('#room')
+room.hidden = true
 
 let roomName
-
-room.hidden = true
+const handleMessageSubmit = (event) => {
+    event.preventDefault()
+    const input = room.querySelector("input")
+    const value = input.value
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`)
+    })
+    input.value = ""
+}
 const showRoom = () => {
     welcome.hidden = true
     room.hidden = false
     const h3 = room.querySelector('h3')
     h3.innerText = `Room: ${roomName}`
+    const form = room.querySelector("form")
+    form.addEventListener("submit", handleMessageSubmit)
 }
 form.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -27,6 +37,9 @@ const addMessage = (msg) => {
     li.innerText = msg
     ul.appendChild(li)
 }
+
 socket.on("welcome", () => {
-    addMessage("Someone Joined")
+    addMessage("Someone Joined 😀")
 })
+socket.on("new_message", addMessage)
+socket.on("bye", () => { addMessage("Someone left 🙁") })
