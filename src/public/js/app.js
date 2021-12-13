10001c59c67c6a6dd37fef421fbd67b89ad3c574
bopboy2 <scp0000@gmail.com>
2021-12-14 08:20:40 +0900

    사용자 비디오 사용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,71 +1,42 @@
 const socket = io()
 
-const welcome = document.querySelector("#welcome")
-const form = welcome.querySelector("form")
-const room = document.querySelector('#room')
-room.hidden = true
+const myFace = document.querySelector("#myFace")
+const muteBtn = document.querySelector("#mute")
+const cameraBtn = document.querySelector("#camera")
 
-let roomName
-const handleMessageSubmit = (event) => {
-    event.preventDefault()
-    const input = room.querySelector("#msg input")
-    const value = input.value
-    socket.emit("new_message", input.value, roomName, () => {
-        addMessage(`You: ${value}`)
-    })
-    input.value = ""
-}
-const handleNicknameSubmit = (event) => {
-    event.preventDefault()
-    const input = room.querySelector("#name input")
-    socket.emit("nickname", input.value)
-}
-const showRoom = () => {
-    welcome.hidden = true
-    room.hidden = false
-    const h3 = room.querySelector('h3')
-    h3.innerText = `Room: ${roomName}`
-    const msgForm = room.querySelector("#msg")
-    msgForm.addEventListener("submit", handleMessageSubmit)
-    const nameForm = room.querySelector("#name")
-    nameForm.addEventListener("submit", handleNicknameSubmit)
-}
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const input = form.querySelector("input")
-    roomName = input.value
-    socket.emit("enter_room", roomName, showRoom)
-    input.value = ""
-})
+let myStream
+let muted = false
+let cameraoff = false
 
-const addMessage = (msg) => {
-    const ul = room.querySelector("ul")
-    const li = document.createElement("li")
-    li.innerText = msg
-    ul.appendChild(li)
-}
-
-socket.on("welcome", (user, count) => {
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName}  (${count})`
-    addMessage(`${user} Joined 😀`)
-})
-socket.on("new_message", addMessage)
-socket.on("bye", (user, count) => {
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName}  (${count})`
-    addMessage(`${user} left 🙁`)
-})
-socket.on("room_change", (rooms) => {
-    const roomList = welcome.querySelector("ul")
-    roomList.innerHTML = ""
-    if (rooms.length === 0) {
-        roomList.innerHTML = ""
-        return
+const getMedia = async () => {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio: true, video: true
+        })
+        myFace.srcObject = myStream
+    } catch (e) {
+        console.log(e)
     }
-    rooms.forEach((room) => {
-        const li = document.createElement("li")
-        li.innerText = room
-        roomList.append(li)
-    })
-})
+}
+getMedia()
+
+const handleMuteClick = () => {
+    if (!muted) {
+        muteBtn.innerText = "Unmute"
+        muted = true
+    } else {
+        muteBtn.innerText = "Mute"
+        muted = false
+    }
+}
+muteBtn.addEventListener("click", handleMuteClick)
+const handleCameraClick = () => {
+    if (cameraoff) {
+        cameraBtn.innerText = "Turn Camera Off"
+        cameraoff = false
+    } else {
+        cameraBtn.innerText = "Turn Camera On"
+        cameraoff = true
+    }
+}
+cameraBtn.addEventListener("click", handleCameraClick)
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import http from 'http'
-import { Server } from 'socket.io'
-import { instrument } from '@socket.io/admin-ui'
+import SocketIO from 'socket.io'
 
 const app = express()
 
@@ -12,53 +11,8 @@ app.get("/", (req, res) => res.render("home"))
 app.get("/*", (req, res) => res.redirect("/"))
 
 const httpServer = http.createServer(app)
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true
-    }
-})
-instrument(wsServer, { auth: false })
+const wsServer = SocketIO(httpServer)
 
 httpServer.listen(3000, () => {
     console.log(`Listening on http://localhost:3000`)
-})
-
-const publicRooms = () => {
-    const { sockets: { adapter: { sids, rooms } } } = wsServer
-    const publicRooms = []
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) { publicRooms.push(key) }
-    })
-    return publicRooms
-}
-const countRoom = (roomName) => {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size
-}
-
-wsServer.on('connection', (socket) => {
-    socket["nickname"] = "Anonymous"
-    socket.onAny(event => {
-        console.log(wsServer.sockets.adapter)
-        console.log(`socket event: ${event}`)
-    })
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName)
-        done()
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName))
-        wsServer.sockets.emit("room_change", publicRooms())
-    })
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1))
-    })
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms())
-    })
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`)
-        done()
-    })
-    socket.on("nickname", (nickname) => {
-        socket["nickname"] = nickname
-    })
 })
