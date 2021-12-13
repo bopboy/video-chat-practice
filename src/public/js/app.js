335d0f6261d241627f2f695646afd9b8a9859caf
bopboy2 <scp0000@gmail.com>
2021-12-14 06:53:52 +0900

    닉네임 설정해서 채팅

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,20 +8,27 @@ room.hidden = true
 let roomName
 const handleMessageSubmit = (event) => {
     event.preventDefault()
-    const input = room.querySelector("input")
+    const input = room.querySelector("#msg input")
     const value = input.value
     socket.emit("new_message", input.value, roomName, () => {
         addMessage(`You: ${value}`)
     })
     input.value = ""
 }
+const handleNicknameSubmit = (event) => {
+    event.preventDefault()
+    const input = room.querySelector("#name input")
+    socket.emit("nickname", input.value)
+}
 const showRoom = () => {
     welcome.hidden = true
     room.hidden = false
     const h3 = room.querySelector('h3')
     h3.innerText = `Room: ${roomName}`
-    const form = room.querySelector("form")
-    form.addEventListener("submit", handleMessageSubmit)
+    const msgForm = room.querySelector("#msg")
+    msgForm.addEventListener("submit", handleMessageSubmit)
+    const nameForm = room.querySelector("#name")
+    nameForm.addEventListener("submit", handleNicknameSubmit)
 }
 form.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -38,8 +45,8 @@ const addMessage = (msg) => {
     ul.appendChild(li)
 }
 
-socket.on("welcome", () => {
-    addMessage("Someone Joined 😀")
+socket.on("welcome", (user) => {
+    addMessage(`${user} Joined 😀`)
 })
 socket.on("new_message", addMessage)
-socket.on("bye", () => { addMessage("Someone left 🙁") })
+socket.on("bye", (user) => { addMessage(`${user} left 🙁`) })
--- src/server.js
+++ src/server.js
@@ -20,17 +20,21 @@ httpServer.listen(3000, () => {
 
 const sockets = []
 wsServer.on('connection', (socket) => {
+    socket["nickname"] = "Anonymous"
     socket.onAny(event => console.log(`socket event: ${event}`))
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName)
         done()
-        socket.to(roomName).emit("welcome")
+        socket.to(roomName).emit("welcome", socket.nickname)
     })
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye"))
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname))
     })
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg)
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`)
         done()
+    })
+    socket.on("nickname", (nickname) => {
+        socket["nickname"] = nickname
     })
 })
