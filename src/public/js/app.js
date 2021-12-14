78796b349292c32c288cc016977d334dbf03651f
bopboy2 <scp0000@gmail.com>
2021-12-14 10:09:51 +0900

    ice candidate 결정, 비디오 채팅 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -102,17 +102,38 @@ socket.on("welcome", async () => {
 })
 
 socket.on("offer", async (offer) => {
+    console.log("received the offer")
     myPeerConnection.setRemoteDescription(offer)
     const answer = await myPeerConnection.createAnswer()
     myPeerConnection.setLocalDescription(answer)
     socket.emit("answer", answer, roomName)
+    console.log("sent the answer")
 })
 
 socket.on("answer", (answer) => {
+    console.log("received the answer")
     myPeerConnection.setRemoteDescription(answer)
 })
+
+socket.on("ice", ice => {
+    console.log("received the candidate")
+    myPeerConnection.addIceCandidate(ice)
+})
 // RTC code
+const handleIce = (data) => {
+    console.log("sent the candidate")
+    socket.emit("ice", data.candidate, roomName)
+}
+const handleAddStream = (data) => {
+    const peerStream = document.querySelector("#peerFace")
+    peerStream.srcObject = data.stream
+    // console.log("got an stream from my peer")
+    // console.log("Peer's Stream", data.stream)
+    // console.log("My Stream", myStream)
+}
 const makeConnection = () => {
     myPeerConnection = new RTCPeerConnection()
+    myPeerConnection.addEventListener("icecandidate", handleIce)
+    myPeerConnection.addEventListener("addstream", handleAddStream)
     myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream))
 }
