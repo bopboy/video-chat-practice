02d7281280ffea53e1ff42cd93d5331c7c001f6b
bopboy2 <scp0000@gmail.com>
2021-12-14 10:23:20 +0900

    stun 서버 설정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -137,7 +137,17 @@ const handleAddStream = (data) => {
     // console.log("My Stream", myStream)
 }
 const makeConnection = () => {
-    myPeerConnection = new RTCPeerConnection()
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302"
+                ]
+            }
+        ]
+    })
     myPeerConnection.addEventListener("icecandidate", handleIce)
     myPeerConnection.addEventListener("addstream", handleAddStream)
     myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream))
